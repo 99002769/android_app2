f990fc1da3271b7d29759a6ca1208cb3029ab2be
Divya Preethaa <divyapreethaakannan@gmail.com>
2020-11-12 15:07:50 +0530

    Day 4 session 2 content-providers part 1, List view displays sms

--- app/src/main/java/com/example/app2/MyAdapter.java
+++ app/src/main/java/com/example/app2/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.example.app2;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,24 +10,28 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.app2.database.FeedReaderContract;
+
 /**
- * put data into each row of the listview
+ * MyAdapter job is to put data into each row of the listview
  */
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.WordViewHolder> {
+    //  String[]
+    Cursor languagesCursor;
+    LayoutInflater layoutInflater;
+    int titleIndex,subtitleIndex;
 
-   String[] languages;
-   LayoutInflater layoutInflater;
-
-    public MyAdapter(Context context, String[] languagesData) {
-        languages = languagesData;
+    public MyAdapter(Context context, Cursor cursor){
+        //String[] languagesData) {
+        languagesCursor = cursor;
         layoutInflater = LayoutInflater.from(context);
+        titleIndex = languagesCursor.getColumnIndexOrThrow(FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE);
+        subtitleIndex = languagesCursor.getColumnIndexOrThrow(FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE);
     }
 
-
-
     /**
-     * 2
-     * buy wooden plants
+     * 2. yash
+     * onCreateViewHolder  job is to buy wooden planks
      * @param parent
      * @param viewType
      * @return
@@ -39,34 +44,49 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.WordViewHolder> {
     }
 
     /**
-     * 3
-     * write data on the planks
+     * 3. jyothsana
+     * onBindViewHolder job is write data on the planks
      * @param holder
      * @param position
      */
     @Override
     public void onBindViewHolder(@NonNull MyAdapter.WordViewHolder holder, int position) {
-        holder.titleTextView.setText(languages[position]);
+        if(position+1 < languagesCursor.getCount()) {
+            languagesCursor.move(position + 1);
+            String title = languagesCursor.getString(titleIndex);
+            String subtitle = languagesCursor.getString(subtitleIndex);
+
+            holder.titleTextView.setText(title);
+            holder.subtitleTextView.setText(subtitle);
+        }
+        //languages[position]);
     }
 
     /**
-     * 1
-     * keep the count of number of data items on the dataset
+     * 1. sandeep
+     * job is to keep the count of no of data items in the dataset
      * @return
      */
     @Override
     public int getItemCount() {
-        return languages.length;
+        return languagesCursor.getCount()+1;
+        //languagesCursor.length;
     }
 
+
     /**
-     * hold the recycled stock and new stock of wooden planks
+     * manasa
+     * WordViewHolder job is to hold the recycled stock and new stock of wooden planks
      */
     public class WordViewHolder extends RecyclerView.ViewHolder {
-        public TextView titleTextView;
+        public  TextView titleTextView;
+        public  TextView subtitleTextView;
+
         public WordViewHolder(@NonNull View itemView) {
             super(itemView);
             titleTextView = itemView.findViewById(R.id.textviewRow);
+            subtitleTextView = itemView.findViewById(R.id.textViewsubtitle);
+
         }
     }
 }
